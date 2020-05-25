4e86a4d05ec8f7fe4648c294da476f14e08e83b6
bayashi <bayashi@cpan.org>
2020-05-25 14:05:04 +0000

    Add JP ItemInfo example

---
+++ example/jp_iteminfo.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Amazon::PAApi5::Payload;
+use Amazon::PAApi5::Signature;
+use HTTP::Headers;
+use LWP::UserAgent;
+use Data::Dumper;
+
+my $PARTNER_TAG = 'YOUR_ASSOCIATE_PARTNER_TAG-22';
+my $ACCESS_KEY = 'YOUR_ACCESS_KEY';
+my $SECRET_KEY = 'YOUR_SECRET_KEY';
+
+{
+    my $asin = '4873118247';
+
+    my $payload = Amazon::PAApi5::Payload->new(
+        $PARTNER_TAG,
+        'www.amazon.co.jp',
+    )->to_json({
+        ItemIds   => [$asin],
+        Resources   => [qw/
+            ItemInfo.Title
+        /],
+    });
+
+    my $sig = Amazon::PAApi5::Signature->new(
+        $ACCESS_KEY,
+        $SECRET_KEY,
+        $payload,
+        {
+            operation => 'GetItems',
+            host      => 'webservices.amazon.co.jp',
+            region    => 'us-west-2',
+        },
+    );
+
+    my $ua = LWP::UserAgent->new(
+        default_headers => HTTP::Headers->new($sig->headers),
+    );
+
+    my $res = $ua->post($sig->req_url, Content => $sig->payload);
+
+    warn Dumper($res->status_line, $res->content);
+}
+
